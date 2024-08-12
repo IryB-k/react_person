3839d16d5a365f401cd624f6131e01f988366ede
Iryna Botulinska <66320585+IryB-k@users.noreply.github.com>
2024-08-12 19:32:13 +0200

    Update src/components/Person/Person.jsx
    Co-authored-by: Denys Semtso <70220667+etojeDenys@users.noreply.github.com>

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -12,7 +12,9 @@ export const Person = ({ person }) => {
     <section className="Person">
       <h2 className="Person__name">My name is {name}</h2>
       {age && <p className="Person__age">I am {age}</p>}
+     <p className="Person__partner">
       {marriedStatus}
+    </p>
     </section>
   );
 };
